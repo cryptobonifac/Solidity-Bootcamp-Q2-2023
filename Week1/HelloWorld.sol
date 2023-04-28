9ec80e5284bf748958ebe2d89f320272485f6e9f
cryptobonifac <maro10@protonmail.com>
2023-04-28 13:21:39 +0200

    playing with NatSpec comments

--- Week1/HelloWorld.sol
+++ Week1/HelloWorld.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
+//@title Hello world contract
+//@notice be careful lol
 contract HelloWorld {
     string private text;
     address public owner;
 
+//@notice initialize contract and sets owner
     constructor() {
         text = "Hello World";
         owner = msg.sender;
@@ -19,12 +22,15 @@ contract HelloWorld {
         return text;
     }
 
+//@notice sets text to new value,restricted to owner of the contract
+//@param new text
     function setText(string calldata newText) public onlyOwner {
         text = newText;
     }
 
+//@notice transfers ownership of the contract,restricted to owner of the contract
+//@param address of new owner
     function transferOwnership(address newOwner) public onlyOwner {
         owner = newOwner;
-    }
-    
+    }   
 }
