ce8bffde57dbb83bf5f574c753140adcd9f331ee
palmcivet7 <palmcivet@proton.me>
2023-05-15 15:30:01 +0100

    Update interact-tokenized-ballot.ts
    added process.env.PRIVATE_KEY for signing

--- Week3/week3-tokenized-ballot/scripts/interact-tokenized-ballot.ts
+++ Week3/week3-tokenized-ballot/scripts/interact-tokenized-ballot.ts
@@ -1,43 +1,49 @@
 import { ethers } from "hardhat";
+require("dotenv").config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL;
 
 // Below addresses are deployed on Mumbai
 const tokenizedBallotAddress = "0x7Aa635a92b0E89fF6118A9D2e3c0fCb01A81bBCb";
 const myERC20VotesAddress = "0x624f15bA871F6A7b2A78d1Cde8AE4168302FAc72";
 
 // Set the voter, delegate and proposal index
-const voterAddress = "0x789...";
-const delegateAddress = "0xabc...";
+const voterAddress = "0x777452cBc7E71B5286e60BA935292FFd49a597A5"; // Change voter and delegate address as appropriate
+const delegateAddress = "0x777452cBc7E71B5286e60BA935292FFd49a597A5";
 const proposalIndex = 1;
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
+  // Create a signer from the private key
+  const provider = new ethers.providers.JsonRpcProvider(MUMBAI_RPC_URL);
+  const signer = new ethers.Wallet(PRIVATE_KEY, provider);
 
   // Connect to the contracts
   const TokenizedBallot = await ethers.getContractFactory("TokenizedBallot");
-  const tokenizedBallot = TokenizedBallot.attach(tokenizedBallotAddress);
+  const tokenizedBallot = TokenizedBallot.connect(signer).attach(
+    tokenizedBallotAddress
+  );
 
   const MyERC20Votes = await ethers.getContractFactory("MyERC20Votes");
-  const myERC20Votes = MyERC20Votes.attach(myERC20VotesAddress);
+  const myERC20Votes = MyERC20Votes.connect(signer).attach(myERC20VotesAddress);
 
   // Give voting tokens to the voter address
-  const mintAmount = ethers.utils.parseUnits("1000", 18);
-  await myERC20Votes.connect(deployer).mint(voterAddress, mintAmount);
+  const mintAmount = ethers.utils.parseUnits("100", 18);
+  await myERC20Votes.mint(voterAddress, mintAmount);
   console.log(`Minted ${mintAmount.toString()} tokens to ${voterAddress}`);
 
   // Delegate voting power to another address
-  await myERC20Votes.connect(voterAddress).delegate(delegateAddress);
+  await myERC20Votes.connect(signer).delegate(delegateAddress);
   console.log(
     `Delegated voting power from ${voterAddress} to ${delegateAddress}`
   );
 
-  // Cast a vote on the TokenizedBallot
-  const voteAmount = ethers.utils.parseUnits("500", 18);
-  await tokenizedBallot
-    .connect(delegateAddress)
-    .vote(proposalIndex, voteAmount);
-  console.log(
-    `Vote cast for proposal ${proposalIndex} with ${voteAmount.toString()} tokens by ${delegateAddress}`
-  );
+  // // Cast a vote on the TokenizedBallot
+  // const voteAmount = ethers.utils.parseUnits("1", 18);
+  // await tokenizedBallot.connect(signer).vote(proposalIndex, voteAmount);
+  // console.log(
+  //   `Vote cast for proposal ${proposalIndex} with ${voteAmount.toString()} tokens by ${delegateAddress}`
+  // );
 
   // Check the voting power of the delegate address
   const votingPower = await myERC20Votes.getPastVotes(
