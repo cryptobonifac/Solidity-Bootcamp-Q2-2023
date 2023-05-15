dbc19e7b640a2ae3a7362487faf6aedf52fb07b3
palmcivet7 <palmcivet@proton.me>
2023-05-15 15:39:15 +0100

    Update interact-tokenized-ballot.ts
    change voter and delegate addresses

--- Week3/week3-tokenized-ballot/scripts/interact-tokenized-ballot.ts
+++ Week3/week3-tokenized-ballot/scripts/interact-tokenized-ballot.ts
@@ -9,8 +9,8 @@ const tokenizedBallotAddress = "0x7Aa635a92b0E89fF6118A9D2e3c0fCb01A81bBCb";
 const myERC20VotesAddress = "0x624f15bA871F6A7b2A78d1Cde8AE4168302FAc72";
 
 // Set the voter, delegate and proposal index
-const voterAddress = "0x777452cBc7E71B5286e60BA935292FFd49a597A5"; // Change voter and delegate address as appropriate
-const delegateAddress = "0x777452cBc7E71B5286e60BA935292FFd49a597A5";
+const voterAddress = "0x63Ec8bcF66479CE5844Eb8cb5147C9D1CC448B95"; // Change voter and delegate address as appropriate
+const delegateAddress = "0x9848d2ae3f54e0502aa74b6a24aa011ac28a1d28";
 const proposalIndex = 1;
 
 async function main() {
